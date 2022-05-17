3a4bbc9fdb21131b524426717129e594cc06ba32
BasawarajBiradar <basawarajbiradar007@gmail.com>
2022-05-17 10:57:30 +0530

    components and directives and pipes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FoooterComponent } from './foooter/foooter.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
+import { HighlightDirective } from './highlight.directive';
+import { ImgurlPipe } from './imgurl.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FoooterComponent
+    FoooterComponent,
+    MainContainerComponent,
+    UserListComponent,
+    UserComponent,
+    HighlightDirective,
+    ImgurlPipe
   ],
   imports: [
     BrowserModule
